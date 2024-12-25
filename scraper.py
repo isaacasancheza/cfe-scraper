e4c53559ea5551a9d0b86b3c0b05a7e6341d9a41
Isaac Sánchez <1031819+isaacasancheza@users.noreply.github.com>
2024-12-25 00:05:37 -0600

    :art: format code

--- scraper.py
+++ scraper.py
@@ -1,24 +1,26 @@
-#!/usr/bin/env python3
-from re import search
-from math import inf
-from json import dumps
-from typing import Any, TypedDict
-from decimal import Decimal
-from datetime import date
+#!/usr/bin/env -S uv run
 from argparse import ArgumentParser
+from datetime import date
+from decimal import Decimal
+from json import dumps
+from math import inf
+from re import search
+from typing import Any, TypedDict
 
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
-DEFAULT_URL = 'https://app.cfe.mx/Aplicaciones/CCFE/Tarifas/TarifasCRECasa/Tarifas/Tarifa1C.aspx'
+DEFAULT_URL = (
+    'https://app.cfe.mx/Aplicaciones/CCFE/Tarifas/TarifasCRECasa/Tarifas/Tarifa1C.aspx'
+)
 
 
 class Rate(TypedDict):
     kWh: float
     name: str
     price: Decimal
-    
+
 
 class SummerMonth(TypedDict):
     month: int
@@ -45,7 +47,7 @@ class Scraper:
 
         self._url = url
         self._driver = Chrome(options=options)
-        
+
     def scrap(self, year: int, /) -> Data:
         data: Data = {
             'year': year,
@@ -64,7 +66,7 @@ class Scraper:
                     self._select_month(month)
                     summer_month: SummerMonth = {
                         'month': month,
-                        'rates': self._get_rates() 
+                        'rates': self._get_rates(),
                     }
                     summer['months'].append(summer_month)
                 data['summers'].append(summer)
@@ -74,7 +76,9 @@ class Scraper:
 
     def _get_rates(self) -> list[Rate]:
         rates: list[Rate] = []
-        tbody = self._driver.find_element(By.XPATH, '//b[contains(text(), "Consumo básico")]/../../..')
+        tbody = self._driver.find_element(
+            By.XPATH, '//b[contains(text(), "Consumo básico")]/../../..'
+        )
         for tr in tbody.find_elements(By.XPATH, './tr'):
             name, price, kWh = tr.find_elements(By.XPATH, './td')
 
@@ -85,7 +89,7 @@ class Scraper:
                 kWh = inf
             name = name.text.strip()
             price = Decimal(price.text.strip())
-            
+
             rates.append(
                 {
                     'kWh': kWh,
@@ -94,23 +98,23 @@ class Scraper:
                 }
             )
         return rates
-    
+
     def _select_year(self, year: int, /) -> None:
         xpath = '//span[contains(text(), "Consultar tarifas de:")]/../following-sibling::td/select'
         self._select(xpath, year)
-    
+
     def _select_summer(self, summer_month: int, /) -> None:
         xpath = '//td[contains(text(), "Elige el mes en que comienza el verano en tu localidad")]/following-sibling::td/select'
         self._select(xpath, summer_month)
-        
+
     def _select_month(self, month: int, /) -> None:
         xpath = '//td[contains(text(), "Elige el mes que deseas consultar")]/following-sibling::td/select'
         self._select(xpath, month)
-    
+
     def _select(self, xpath: str, value: Any, /) -> None:
         select = self._get_select(xpath)
         select.select_by_value(str(value))
-    
+
     def _get_select(self, xpath: str, /) -> Select:
         element = self._driver.find_element(By.XPATH, xpath)
         return Select(element)
@@ -118,15 +122,15 @@ class Scraper:
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    
+
     parser.add_argument('--url', type=str, default=DEFAULT_URL)
     parser.add_argument('--year', type=int, default=date.today().year)
 
     args = parser.parse_args()
-    
+
     url = args.url
     year = args.year
 
     scraper = Scraper(url)
-    
+
     print(dumps(scraper.scrap(year), default=str, indent=4))
